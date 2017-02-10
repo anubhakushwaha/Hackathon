2ebde074d3d9a87961c9e6f1dad517bb4debd7e7
Priyal Aggarwal <priyalaggarwal1@yahoo.in>
2017-02-11 00:36:15 +0530

    Information foldable list

---
+++ GoogleMapsRetrofit/app/src/main/java/com/androidtutorialpoint/googlemapsretrofit/BaseActivity.java
@@ -0,0 +1,33 @@
+package com.androidtutorialpoint.googlemapsretrofit;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ActionBar getSupportActionBar() {
+        // Making getSupportActionBar() method to be @NonNull
+        ActionBar actionBar = super.getSupportActionBar();
+        if (actionBar == null) {
+            throw new NullPointerException("Action bar was not initialized");
+        }
+        return actionBar;
+    }
+
+}
