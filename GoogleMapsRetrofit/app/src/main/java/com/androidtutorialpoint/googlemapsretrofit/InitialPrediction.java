053ebfb087503a0c40c37ee95bc91f45f2ee1a55
HimanshiJain <himshijain@gmail.com>
2017-02-11 16:44:15 +0530

    Prediction result and linked to next activity

--- GoogleMapsRetrofit/app/src/main/java/com/androidtutorialpoint/googlemapsretrofit/InitialPrediction.java
+++ GoogleMapsRetrofit/app/src/main/java/com/androidtutorialpoint/googlemapsretrofit/InitialPrediction.java
@@ -1,10 +1,12 @@
 package com.androidtutorialpoint.googlemapsretrofit;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class InitialPrediction extends AppCompatActivity {
 
@@ -12,7 +14,8 @@ public class InitialPrediction extends AppCompatActivity {
     int sga = 1, height, age, parity;
     int smoker, alcohol;
     double pred;
-    Button predict;
+    Button predict, next;
+    TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +26,7 @@ public class InitialPrediction extends AppCompatActivity {
         parityE = (EditText) findViewById(R.id.parity);
         smokerE = (EditText) findViewById(R.id.smoke);
         alcoholE = (EditText) findViewById(R.id.alcohol);
+        result = (TextView) findViewById(R.id.result);
         predict.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,7 +43,15 @@ public class InitialPrediction extends AppCompatActivity {
 
                 pred = (double )height*(-8.355455425669245859e-03)+(double)age*(-6.924575061266355358e-03)
                         +(double )sga*(-1.528999369474286940e+00)+(double)parity*(-3.018319793416523664e-02)+(double)smoker*(1.848600037062445023e-01) ;
-                
+                result.setText("Your predicted gestation period would last for : "+ (int)pred + " weeks");
+            }
+        });
+        next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), UserMedicineSettingsFragmentActivity.class);
+                startActivity(i);
             }
         });
     }
